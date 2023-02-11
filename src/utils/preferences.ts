c9fb3fad47d6f89e0d71d0b6b459cf0b45acf0d3
Ali Parandeh <ali@aparandeh.com>
2023-02-11 17:56:36 +0000

    fix: remove spaceId

--- src/utils/preferences.ts
+++ src/utils/preferences.ts
@@ -3,7 +3,6 @@ import { getPreferenceValues } from "@raycast/api";
 interface Preferences {
   token: string;
   orgName: string;
-  spaceId: string;
 }
 
 const preferences: Preferences = getPreferenceValues();
